c40b9ff4ce344db8dae3ac3043407619892b1e99
Laura Trager <laura.trager@gmail.com>
2017-11-15 19:02:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,9 +35,9 @@ class Song
    Song.all.detect {|song| song.name == "#{name}"}
   end
 
-  def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      nil
+  ef self.find_or_create_by_name(name)
+    if song = self.find_by_name(name)
+      return song
     else
       Song.create_by_name(name)
     end
