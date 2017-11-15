4ceff2a5c2e516e04a3d8042d183b6d4ad4cb41d
Laura Trager <laura.trager@gmail.com>
2017-11-15 20:11:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,5 +58,14 @@ class Song
     return song
   end
 
+  def self.create_from_filename(mp3)
+   song = self.new_from_filename(mp3)
+   if self.find_by_name(name)
+     return song
+   else
+     song = self.new_from_filename(mp3)
+     song.save
+   end
+ end
 
 end
